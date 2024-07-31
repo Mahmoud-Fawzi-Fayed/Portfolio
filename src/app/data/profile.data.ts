f15870447dd127d3aa2fa58d326203ce03331cfc
Mahmoud-Fawzi-Fayed <Mahmoud-Fawzi-Fayed>
2024-07-31 14:33:09 +0300

    Resume_update

--- src/app/data/profile.data.ts
+++ src/app/data/profile.data.ts
@@ -3,7 +3,7 @@ import { ProfileSchema } from "@data/schema/profile.schema";
 const profileData: ProfileSchema = {
   name: 'Mahmoud Fawzy',
   bio: 'Cybersecurity penetration and web development',
-  resumeLink: 'assets/cv.pdf',
+  resumeLink: 'assets/MahmoudFawzyResume.pdf',
   greetings: [
     'Hello, there!',
   ],
